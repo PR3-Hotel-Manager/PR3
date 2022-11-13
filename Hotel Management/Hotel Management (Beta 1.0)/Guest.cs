71e56544b69e83f666c00252329f94175f120031
michaelgranberry <michaelgranberryii@gmail.com>
2022-11-12 21:18:54 -0800

    Added Guest Construstors

--- Hotel Management/Hotel Management (Beta 1.0)/Guest.cs
+++ Hotel Management/Hotel Management (Beta 1.0)/Guest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Hotel_Management__Beta_1._0_
 {
@@ -17,6 +18,27 @@ namespace Hotel_Management__Beta_1._0_
         public Payment payment;
 
         // Conctrustors -------------------------------
+
+        public Guest()
+        {
+            this.FirstName = "";
+            this.LastName = "";
+            this.Age = "";
+            this.Stay = "";
+            this.room = new Room("","",false);
+            this.payment = new Payment(0, "");
+        }
+
+        public Guest(string roomNumber)
+        {
+            this.FirstName = "";
+            this.LastName = "";
+            this.Age = "";
+            this.Stay = "";
+            this.room = new Room(roomNumber, InitBedConfiguration(roomNumber), false);
+            this.payment = new Payment(0, "");
+        }
+
         public Guest(string firstName, string lastName, string age, string stay, Room room, Payment payment)
         {
             this.FirstName = firstName;
@@ -27,5 +49,18 @@ namespace Hotel_Management__Beta_1._0_
             this.payment = payment;
         }
 
+        private string InitBedConfiguration(string roomNumber)
+        {
+            int rNum = Convert.ToInt32(roomNumber);
+            if (rNum <= 15)
+            {
+                return 1.ToString();
+            }
+            else
+            {
+                return 2.ToString();
+
+            }
+        }
     }
 }
---
+++ Hotel Management/Hotel Management (Beta 1.0)/JSONGuestModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Management__Beta_1._0_
+{
+    public class JSONGuestModel
+    {
+        // Attributes -------------------------
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string Stay { get; set; }
+        public JSONRooms room;
+        public JSONPayment payment;
+    }
+
+    public class JSONRooms
+    {
+        public string RoomNumber { get; set; }
+        public string BedConfiguration { get; set; }
+        public bool Occupied { get; set; }
+    }
+
+    public class JSONPayment
+    {
+        public double price { get; set; }
+        public string paymentType { get; set; }
+        public string Time { get; set; }
+    }
+}
