4d3bd666a85710571e35099dafc79c4ee8f7e4c1
michaelgranberry <michaelgranberryii@gmail.com>
2022-12-01 18:00:48 -0800

    added test project

---
+++ Hotel Management/PR3_MS_Test/MainTest.cs
@@ -0,0 +1,12 @@
+using Hotel_Management__Beta_1;
+namespace PR3_MS_Test
+{
+    [TestClass]
+    public class MainTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
