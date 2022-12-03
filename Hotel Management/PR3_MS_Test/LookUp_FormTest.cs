7175837e052f9e0caf4511013bbce760d2534240
RFREM <64663164+RFREM@users.noreply.github.com>
2022-12-02 17:19:04 -0800

    Final Unit Test for look up form

--- Hotel Management/PR3_MS_Test/LookUp_FormTest.cs
+++ Hotel Management/PR3_MS_Test/LookUp_FormTest.cs
@@ -19,16 +19,23 @@ namespace PR3_MS_Test
             main_Form.InitGuestData();
         }
 
-        [TestMethod]
-        public void PerformSearch_ResultOneMatch_ReturnsOne()
+        void addTestGuests()
         {
+            //ADDING FOLLOWING GUESTS:
+            //Ralph Frem 
+            //Ralph Granberry
+            //John Frem 
+            //Joe Frem 
+            //
+            //First Name Return 2
+            //Last Name Return 3
+
             db.StartFirebase();
             setupDB();
             CheckIn_Form checkIn_Form = new CheckIn_Form();
             Payment payment = new Payment();
-            LookUp_Form lookUp_Form = new LookUp_Form(); //New instance
 
-            // Arrange 
+            //ONE FULL MATCH DATA// 
             string firstName = "Ralph";
             string lastName = "Frem";
             string age = "32";
@@ -48,7 +55,82 @@ namespace PR3_MS_Test
                                                     bedConfig,
                                                     price,
                                                     paymentType);
+            //-------END-------//
 
+            //FIRST MATCH DATA//
+            string firstName1 = "Ralph";
+            string lastName1 = "Granberry";
+            string age1 = "33";
+            string stayLength1 = "7";
+            string roomNumber1 = "2";
+            string bedConfig1 = "1";
+            string price1 = payment.CalculatePrice(Convert.ToDecimal(bedConfig1), Convert.ToDecimal(stayLength1)).ToString();
+            string paymentType1 = "Cash";
+            Guest guest1 = new Guest();
+
+            checkIn_Form.performCheckIn(guest1,
+                                                    firstName1,
+                                                    lastName1,
+                                                    age1,
+                                                    stayLength1,
+                                                    roomNumber1,
+                                                    bedConfig1,
+                                                    price1,
+                                                    paymentType1);
+            //-------END-------//
+
+            //LAST NAME MATCH DATA//
+            string firstName2 = "John";
+            string lastName2 = "Frem";
+            string age2 = "34";
+            string stayLength2 = "8";
+            string roomNumber2 = "3";
+            string bedConfig2 = "1";
+            string price2 = payment.CalculatePrice(Convert.ToDecimal(bedConfig2), Convert.ToDecimal(stayLength2)).ToString();
+            string paymentType2 = "Cash";
+            Guest guest2 = new Guest();
+
+            checkIn_Form.performCheckIn(guest2,
+                                                    firstName2,
+                                                    lastName2,
+                                                    age2,
+                                                    stayLength2,
+                                                    roomNumber2,
+                                                    bedConfig2,
+                                                    price2,
+                                                    paymentType2);
+            //-------END-------//
+
+            //LAST NAME MATCH DATA 2//
+            string firstName3 = "Joe";
+            string lastName3 = "Frem";
+            string age3 = "32";
+            string stayLength3 = "6";
+            string roomNumber3 = "4";
+            string bedConfig3 = "1";
+            string price3 = payment.CalculatePrice(Convert.ToDecimal(bedConfig3), Convert.ToDecimal(stayLength3)).ToString();
+            string paymentType3 = "Cash";
+            Guest guest3 = new Guest();
+
+            checkIn_Form.performCheckIn(guest3,
+                                                    firstName3,
+                                                    lastName3,
+                                                    age3,
+                                                    stayLength3,
+                                                    roomNumber3,
+                                                    bedConfig3,
+                                                    price3,
+                                                    paymentType3);
+            //-------END-------//
+        }
+
+        [TestMethod]
+        public void PerformSearch_ResultOneMatch_ReturnsOne()// One match found 
+        {
+
+            LookUp_Form lookUp_Form = new LookUp_Form(); //New instance
+
+            // Arrange 
             string firstNameTest = "Ralph";
             string lastNameTest = "Frem";
             var testVar = 1;
@@ -61,7 +143,7 @@ namespace PR3_MS_Test
         }
 
         [TestMethod]
-        public void PerformSearch_NoMatch_ReturnsZero()
+        public void PerformSearch_NoMatch_ReturnsZero()//0 Matches found 
         {
             LookUp_Form lookUp_Form = new LookUp_Form(); //New instance
 
@@ -74,7 +156,53 @@ namespace PR3_MS_Test
 
             //Assert
             Assert.AreEqual(testVar, result);
+        }
 
+        [TestMethod]
+        public void PerformSearch_FirstNameMatch_ReturnsTwo()//First Name Match found 
+        //GUESTS:
+        //Ralph Frem 
+        //Ralph Granberry
+        //John Frem 
+        //Joe Frem 
+        //First Name Return 2
+        //Last Name Return 3
+        {
+            addTestGuests();
+            LookUp_Form lookUp_Form = new LookUp_Form(); //New instance
+
+            string firstNameTest = "Ralph";
+            string lastNameTest = "";
+            int testVar = 2;
+
+            //Act 
+            int result = lookUp_Form.performSearch(firstNameTest, lastNameTest);
+
+            //Assert
+            Assert.AreEqual(testVar, result);
+        }
+
+        [TestMethod]
+        public void PerformSearch_LastNameMatch_ReturnsThree()//Last Name Match found 
+        //GUESTS:
+        //Ralph Frem 1
+        //Ralph Granberry 2
+        //John Frem 3
+        //Joe Frem 4
+        //First Name Return 2
+        //Last Name Return 3
+        {
+            LookUp_Form lookUp_Form = new LookUp_Form(); //New instance
+
+            string firstNameTest = "";
+            string lastNameTest = "Frem";
+            var testVar = 3;
+
+            //Act 
+            var result = lookUp_Form.performSearch(firstNameTest, lastNameTest);
+
+            //Assert
+            Assert.AreEqual(testVar, result);
         }
     }
 }
